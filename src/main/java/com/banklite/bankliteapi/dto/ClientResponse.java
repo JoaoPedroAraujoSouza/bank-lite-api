e8c5f33c67db252190075b8e8907b23e25877251
JoaoPedroAraujoSouza <joao.pedroaraujosouza@ufrpe.br>
2025-08-22 14:46:51 -0300

    feat: Add Request/Response DTOs for Client entity

---
+++ src/main/java/com/banklite/bankliteapi/dto/ClientResponse.java
@@ -0,0 +1,52 @@
+package com.banklite.bankliteapi.dto;
+
+import java.time.Instant;
+
+public class ClientResponse {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String cpf;
+    private Instant createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+}
