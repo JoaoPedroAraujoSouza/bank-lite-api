1287d0ea93a19942b8c4eda6b1d18b8f1df83257
JoaoPedroAraujoSouza <joao.pedroaraujosouza@ufrpe.br>
2025-08-31 13:20:10 -0300

    feat(security): Implement authentication service, controller, and initial user seed

--- src/main/java/com/banklite/bankliteapi/controller/AuthenticationController.java
+++ src/main/java/com/banklite/bankliteapi/controller/AuthenticationController.java
@@ -1,4 +1,27 @@
 package com.banklite.bankliteapi.controller;
 
+import com.banklite.bankliteapi.dto.auth.LoginRequest;
+import com.banklite.bankliteapi.dto.auth.LoginResponse;
+import com.banklite.bankliteapi.service.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+
 public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authenticationService.login(request));
+    }
 }
--- src/main/java/com/banklite/bankliteapi/service/AuthenticationService.java
+++ src/main/java/com/banklite/bankliteapi/service/AuthenticationService.java
@@ -1,4 +1,44 @@
 package com.banklite.bankliteapi.service;
 
+import com.banklite.bankliteapi.dto.auth.LoginRequest;
+import com.banklite.bankliteapi.dto.auth.LoginResponse;
+import com.banklite.bankliteapi.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthenticationService {
+
+    UserRepository userRepository;
+    JwtService jwtService;
+    AuthenticationManager authenticationManager;
+
+    public AuthenticationService(UserRepository userRepository, JwtService jwtService, AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public LoginResponse login(LoginRequest request) {
+
+        authenticationManager.authenticate (
+                new UsernamePasswordAuthenticationToken (
+                        request.getLogin(),
+                        request.getPassword()
+                )
+        );
+
+        var user = userRepository.findByLogin(request.getLogin())
+                .orElseThrow(() -> new IllegalStateException("User not found"));
+
+        var accessToken = jwtService.generateToken(user);
+        var refreshToken = jwtService.generateRefreshToken(user);
+
+        LoginResponse response = new LoginResponse();
+        response.setAccessToken(accessToken);
+        response.setRefreshToken(refreshToken);
+
+        return response;
+    }
 }
