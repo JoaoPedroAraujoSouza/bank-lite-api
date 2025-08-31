dec4a95bb5c74ce47888944ef58a34cbf6fd4094
JoaoPedroAraujoSouza <joao.pedroaraujosouza@ufrpe.br>
2025-08-31 15:46:38 -0300

    fix(security): Correctly associate ROLE_USER with the initial user seed

--- src/main/resources/db/migration/V4__insert_initial_security_data.sql
+++ src/main/resources/db/migration/V4__insert_initial_security_data.sql
@@ -3,4 +3,8 @@ INSERT INTO roles (id, name) VALUES (2,  'ROLE_USER');
 
 INSERT INTO users (id, login, password) VALUES (1, 'admin', '$2a$12$G9oEh0rRFh.Y.uVFxtUwwe7AXjG2AexCKBaMwCaJl4gXJ5DGLYz7e');
 
+INSERT INTO users (id, login, password) VALUES (2, 'user', '$2a$12$DiMDJzOTXcvsa0dq/t/31OqdqyC4YEF6c2x/JABpAxe/b9EGWMyIG');
+
+INSERT INTO user_roles (user_id, role_id) VALUES (2, 2);
+
 INSERT INTO user_roles (user_id, role_id) VALUES (1, 1);
